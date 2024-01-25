6068f18d55670ce811c3d6779267edef3c8febb8
Gamerplayer22 <kalman.lengyel@trojans.dsu.edu>
2024-01-25 16:31:00 -0600

    file updates and fixes

--- A1/Program.cs
+++ A1/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +11,9 @@ namespace csc_455_1
         static void Main(string[] args)
         {
             int choice = 1;
-            int x = 0;
+            char x;
 
-            while (choice!=1)
+            while (choice != 1)
             {
                 Console.WriteLine("Main menu");
                 Console.WriteLine("1. random number");
@@ -21,13 +21,13 @@ namespace csc_455_1
                 Console.WriteLine("3. list of dinosaurs");
                 Console.WriteLine("4. string magic");
                 Console.WriteLine("5. exit");
-                Cosnole.WriteLine("Choose 1-5:");
-                Console.ReadLine(x);
-                if(x==1) { }
-                if(x==2) { }
-                if (x==3) { }
+                Console.WriteLine("Choose 1-5:");
+                Console.ReadLine();
+                if (x == '1') { }
+                if (x == 2) { }
+                if (x == 3) { }
                 if (x == 4) { }
-                if( x==5) { choice = 0; }
+                if (x == 5) { choice = 0; }
             }
         }
     }
