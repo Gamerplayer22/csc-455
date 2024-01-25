43bbc0c59c1f1aae24663ee7980e96295b31a644
Gamerplayer22 <kalman.lengyel@trojans.dsu.edu>
2024-01-25 17:35:31 -0600

    4th task created

--- A1/Program.cs
+++ A1/Program.cs
@@ -12,7 +12,19 @@ namespace csc_455_1
         {
             int choice = 1;
             string x= "0";
-            int y = 0;
+            string str;
+
+            List<string> dinosaurs = new List<string>();
+            dinosaurs.Add("Gorgosaurus");
+            dinosaurs.Add("Allosaurus");
+            dinosaurs.Add("Aardonyx");
+            dinosaurs.Add("Megalosaurus");
+            dinosaurs.Add("Abrosaurus");
+            dinosaurs.Add("Homalocephale");
+            dinosaurs.Add("Alvarezsaurus");
+            dinosaurs.Add("Datousaurus");
+            dinosaurs.Add("Ammosaurus");
+            dinosaurs.Add("Capitalsaurus");
 
             while (choice == 1)
             {
@@ -45,18 +57,6 @@ namespace csc_455_1
 
                 if (x == "3") 
                 {
-                    List<string> dinosaurs = new List<string>();
-                    dinosaurs.Add("Gorgosaurus");
-                    dinosaurs.Add("Allosaurus");
-                    dinosaurs.Add("Aardonyx");
-                    dinosaurs.Add("Megalosaurus");
-                    dinosaurs.Add("Abrosaurus");
-                    dinosaurs.Add("Homalocephale");
-                    dinosaurs.Add("Alvarezsaurus");
-                    dinosaurs.Add("Datousaurus");
-                    dinosaurs.Add("Ammosaurus");
-                    dinosaurs.Add("Capitalsaurus");
-
                     Console.WriteLine("unordered list: ");
                     foreach (string d in dinosaurs)
                         Console.WriteLine(d);
@@ -74,11 +74,49 @@ namespace csc_455_1
                     Console.WriteLine();
                 }
 
-                if (x == "4") { }
+                if (x == "4") 
+                {
+                    Console.WriteLine("give a string, at least 10 characters: ");
+                    str = Console.ReadLine();
+                    Random rnd= new Random();
+                    Console.WriteLine("something random happens to the string:");
+                    switch (rnd.Next(10))
+                    {
+                        case 0:
+                            Console.WriteLine(str.Reverse());
+                            break;
+                        case 1:
+                            Console.WriteLine(str.Substring(1,3));
+                            break;
+                        case 2:
+                            Console.WriteLine(str.ToUpper());
+                            break;
+                        case 3:
+                            Console.WriteLine(str.ToLower());
+                            break;
+                        case 4:
+                            Console.WriteLine(str.GetHashCode());
+                            break;
+                        case 5:
+                            Console.WriteLine(str.Replace('a','e'));
+                            break;
+                        case 6:
+                            Console.WriteLine(str.Remove(2,4));
+                            break;
+                        case 7:
+                            Console.WriteLine(str.Split());
+                            break;
+                        case 8:
+                            Console.WriteLine("letter 'a' first occurrance: {0}",str.IndexOf('a'));
+                            break;
+                        case 9:
+                            Console.WriteLine("letter 'a' last occurrance: {0}",str.LastIndexOf('a'));
+                            break;
+                    }
+
+                }
                 if (x == "5") { choice = 0; }
             }
-            Console.WriteLine("Press enter to leave...");
-            Console.ReadLine();
         }
     }
 }
