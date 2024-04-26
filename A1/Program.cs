777423a61654042fada4e30ca8e34e3de2d75b31
Gamerplayer22 <kalman.lengyel@trojans.dsu.edu>
2024-04-26 11:18:40 -0500

    error again

--- A1/Program.cs
+++ A1/Program.cs
@@ -72,7 +72,7 @@ namespace csc_455_1
                     Console.WriteLine("Random dinosaur picked: {0}", dinosaurs[rnd.Next(10)]);
                     Console.WriteLine();
                     Console.WriteLine();
-                    Console.WriteLine(;
+                    Console.Wri
                 }
 
                 if (x == "4") 
